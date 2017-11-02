1d16a220c090367627bf9c6deef35a1001c546d4
ajengdhanindita <adhanindita@gmail.com>
2017-11-03 00:08:54 +0700

    text.py

--- doc-to-list-try/text.py
+++ doc-to-list-try/text.py
@@ -1,12 +1,20 @@
 def read_words(words_file):
-    open_file = open(words_file, 'r')
-    words_list = []
-    doc_list = []
-    contents = open_file.readlines()
-    for i in range(len(contents)):
-        words_list.extend(contents[i].split())
-    return words_list
+    table = []
+    open_file = open(words_file, 'r') #read file
+    contents = open_file.readlines() #read every line on file
+    for line in contents: #for each line on contents 
+        words_list = line.split() #split by any whitespaces, then return as array
+        table.append(words_list) #append the last words_list with the current words_list as array 
+    return table
     open_file.close()
 
 print (read_words('text.txt'))
+
+
+#other way:
+# >>> with open('text.txt') as f:
+#...     table_data = [ line.split() for line in f]
+#... 
+#>>> table_data
+#   [['hello', 'there'], ["i'm", 'fine'], ['thank', 'you', 'for', 'your', 'help'], ['good', 'bye!']]
 
