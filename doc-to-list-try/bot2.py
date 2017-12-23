2c554452da555a19457f0bcb968be904114f1d5d
ajengdhanindita <adhanindita@gmail.com>
2017-12-24 01:20:29 +0700

    tryin the bot

--- doc-to-list-try/bot2.py
+++ doc-to-list-try/bot2.py
@@ -9,8 +9,17 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+from telegram.ext import MessageHandler, Filters
+
+
 from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
 
 updater.start_polling()
