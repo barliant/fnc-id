f46a70cf2bf97dfd9a9e0a908e834a88b19835b6
ajengdhanindita <adhanindita@gmail.com>
2017-12-26 10:00:55 +0700

    fixing the bot, but not really fixed

--- doc-to-list-try/bot2.py
+++ doc-to-list-try/bot2.py
@@ -11,26 +11,27 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
-def add_file(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text = "Enter a sentence: ")
-    new_doc = update.message.text
-    if new_doc is not None:
+def addfile(bot, update):
+	bot.send_message(chat_id=update.message.chat_id, text = "Enter a sentence: ")
+	new_doc = update.message.text
+	if new_doc is not None:
 		corpus_handler = MessageHandler(Filters.text, corpus)
 		dispatcher.add_handler(corpus_handler)
 	new_doc is None
 
 def corpus(bot, update):
-   print(update.message.chat_id)
+   	print(update.message.chat_id)
 	new_doc = update.message.text
 	filename = "corpuz.csv"
-    f = open(filename, "w")
-    f.write(new_doc.replace(",", ";") + "\n")
+    	f = open(filename, "w")
+    	f.write(new_doc.replace(",", ";") + "\n")
 	f.close()
+	
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-echo_handler = MessageHandler('add', add_file)
-dispatcher.add_handler(echo_handler)
+addfile_handler = CommandHandler('add', addfile)
+dispatcher.add_handler(addfile_handler)
 
 updater.start_polling()
