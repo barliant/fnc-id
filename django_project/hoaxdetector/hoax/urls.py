f04c4153e66bdd1682d2dc23f1354da36003a6fe
ajengdhanindita <adhanindita@gmail.com>
2018-01-16 01:05:57 +0700

    edit django web interface

--- django_project/hoaxdetector/hoax/urls.py
+++ django_project/hoaxdetector/hoax/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index),
+	url(r'^checkhoax$', views.checkhoax),
+	url(r'^addcorpus$', views.addcorpus),
+	url(r'^viewcorpus$', views.viewcorpus),
 ]
--- django_project/hoaxdetector/hoax/views.py
+++ django_project/hoaxdetector/hoax/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request, 'hoax/index.html')
+
+def checkhoax(request):
+	return render(request, 'hoax/checkhoax.html')
+
+def addcorpus(request):
+	return render(request, 'hoax/addcorpus.html')
+
+def viewcorpus(request):
+	return render(request, 'hoax/viewcorpus.html')
